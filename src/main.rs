eddd3021c8d1e015dbb5c3023d9e53b229f0b820
Kare Lehtonen <lehtonenkare@gmail.com>
2023-10-26 11:43:01 +0300

    Added main.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate lazy_static;
+use lazy_static::lazy_static;
+use crate::configuration::Configuration;
+
+mod configuration;
+mod user;
+mod user_service;
+mod user_controller;
+mod user_client;
+
+lazy_static! {
+    static ref CONFIG: Configuration = Configuration::read_from_config_file("resources").unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Hello world!");
+}
